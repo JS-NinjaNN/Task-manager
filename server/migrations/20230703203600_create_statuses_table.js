d7ea8f3111469947e7f27a102ee6c45292f50911
Artem Moiseenko <moiseenko.artem94@yandex.ru>
2023-07-18 18:58:12 +0300

    update statuses migration

--- server/migrations/20230703203600_create_statuses_table.js
+++ server/migrations/20230703203600_create_statuses_table.js
@@ -5,7 +5,7 @@
 export const up = (knex) => (
   knex.schema.createTable('statuses', (table) => {
     table.increments('id').primary();
-    table.string('name').notNullable();
+    table.string('name').unique().notNullable();
     table.timestamp('created_at').defaultTo(knex.fn.now());
     table.timestamp('updated_at').defaultTo(knex.fn.now());
   })
