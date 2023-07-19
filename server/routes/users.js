e06c81f504ee4d2487a2c7c9ec7bfb0ddf17140c
Artem Moiseenko <moiseenko.artem94@yandex.ru>
2023-07-19 19:44:11 +0300

    update users route

--- server/routes/users.js
+++ server/routes/users.js
@@ -14,29 +14,27 @@ export default (app) => {
       reply.render('users/new', { user });
       return reply;
     })
-    .get('/users/:id/edit', async (req, reply) => {
+    .get('/users/:id/edit', { name: 'editUser', preValidation: app.authenticate }, async (req, reply) => {
       const { id } = req.params;
-      if (req.isAuthenticated()) {
-        const currentUserId = req.user.id;
-        if (Number(id) !== currentUserId) {
-          req.flash('error', i18next.t('flash.wrongUserError'));
-          reply.render('welcome/index');
-        } else {
-          const user = await app.objection.models.user.query().findById(id);
-          reply.render('users/edit', { user });
-        }
-      } else {
-        req.flash('error', i18next.t('flash.authError'));
-        reply.render('welcome/index');
+      const currentUserId = req.user.id;
+
+      if (Number(id) !== currentUserId) {
+        req.flash('error', i18next.t('flash.wrongUserError'));
+        return reply.redirect(app.reverse('users'));
       }
+
+      const user = await app.objection.models.user.query().findById(id);
+      reply.render('users/edit', { user });
+
       return reply;
     })
     .post('/users', async (req, reply) => {
       const user = new app.objection.models.user();
-      user.$set(req.body.data);
+      const { data } = req.body;
+      user.$set(data);
 
       try {
-        const validUser = await app.objection.models.user.fromJson(req.body.data);
+        const validUser = await app.objection.models.user.fromJson(data);
         await app.objection.models.user.query().insert(validUser);
         req.flash('info', i18next.t('flash.users.create.success'));
         reply.redirect(app.reverse('root'));
@@ -46,42 +44,49 @@ export default (app) => {
       }
       return reply;
     })
-    .patch('/users/:id', async (req, reply) => {
+    .patch('/users/:id', { name: 'editUser', preValidation: app.authenticate }, async (req, reply) => {
       const { id } = req.params;
+      const { data } = req.body;
       const user = await app.objection.models.user.query().findById(id);
 
       try {
-        await user.$query().patch(req.body.data);
+        const validUser = await app.objection.models.user.fromJson(data);
+        await user.$query().patch(validUser);
         req.flash('info', i18next.t('flash.users.edit.success'));
         reply.redirect(app.reverse('users'));
       } catch (errors) {
         req.flash('error', i18next.t('flash.users.edit.error'));
-        reply.code(422);
         user.$set(req.body.user);
         reply.render('users/edit', { user, errors: errors.data ?? {} });
       }
 
       return reply;
     })
-    .delete('/users/:id', async (req, reply) => {
+    .delete('/users/:id', { name: 'deleteUser', preValidation: app.authenticate }, async (req, reply) => {
       const { id } = req.params;
-      if (req.isAuthenticated()) {
-        const currentUserId = req.user.id;
-        const relatedTasks = await app.objection.models.task.query().where('executorId', id).orWhere('creatorId', id);
-        if (currentUserId !== Number(id)) {
-          req.flash('error', i18next.t('flash.wrongUserError'));
-          reply.render('welcome/index');
-        } else if (relatedTasks.length > 0) {
-          req.flash('error', i18next.t('flash.authError'));
-          reply.render('welcome/index');
-        } else {
-          await app.objection.models.user.query().deleteById(id);
-          req.logOut();
-          reply.redirect(app.reverse('root'));
-        }
-      } else {
-        req.flash('error', i18next.t('flash.authError'));
-        reply.render('welcome/index');
+      const currentUserId = req.user.id;
+
+      if (currentUserId !== Number(id)) {
+        req.flash('error', i18next.t('flash.wrongUserError'));
+        return reply.redirect(app.reverse('users'));
+      }
+
+      // const relatedTasks = await app.objection.models.task.query().where('executorId', id).orWhere('creatorId', id);
+
+      // if (relatedTasks.length) {
+      //   req.flash('error', i18next.t('flash.authError'));
+      //   reply.render('welcome/index');
+      //   return reply;
+      // }
+
+      try {
+        await app.objection.models.user.query().deleteById(id);
+        req.logOut();
+        req.flash('info', i18next.t('flash.users.delete.success'));
+        reply.redirect(app.reverse('users'));
+      } catch (errors) {
+        req.flash('error', i18next.t('flash.users.delete.error'));
+        reply.redirect(app.reverse('users'));
       }
 
       return reply;
