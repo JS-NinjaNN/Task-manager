31fcec3eab75c0892206f4fef06fea3c7f58b3da
JS-NinjaNN <moiseenko.artem94@yandex.ru>
2023-07-25 13:09:14 +0300

    fix eslint problems

--- server/migrations/20230715212500_create_tasks_labels_table.js
+++ server/migrations/20230715212500_create_tasks_labels_table.js
@@ -3,10 +3,10 @@
 export const up = (knex) => (
   knex.schema.createTable('tasks_labels', (table) => {
     table.increments('id').primary();
-    table.integer('task_id')
-      .unsigned().references('id').inTable('tasks').onDelete('CASCADE');
-    table.integer('label_id')
-      .unsigned().references('id').inTable('labels').onDelete('CASCADE');
+    table.integer('task_id').unsigned().references('id').inTable('tasks')
+      .onDelete('CASCADE');
+    table.integer('label_id').unsigned().references('id').inTable('labels')
+      .onDelete('CASCADE');
     table.unique(['task_id', 'label_id']);
   })
 );
